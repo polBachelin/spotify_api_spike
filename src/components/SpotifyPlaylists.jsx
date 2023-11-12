c913b0b6f3380314b27effff3f90f8961556f31e
Pol Bachelin <pol.bachelin@epitech.eu>
2023-11-12 20:22:31 +0100

    changed from foreach to every

--- src/components/SpotifyPlaylists.jsx
+++ src/components/SpotifyPlaylists.jsx
@@ -57,14 +57,19 @@ function SpotifyPlaylist() {
         a.features[AUDIO_OPTIONS[audioFeature]]
     )
     console.log(pD)
-    pD.forEach((item) => {
-      spotifyApi.queue(item.track).catch((err) => {
-        alert(
-          "It did not find a spotify player, make sure that you have spotify running"
-        )
-        console.log(err)
-        return
-      })
+    pD.every((item) => {
+      spotifyApi
+        .queue(item.track)
+        .then((resp) => {
+          return true
+        })
+        .catch((err) => {
+          alert(
+            "It did not find a spotify player, make sure that you have spotify running"
+          )
+          console.log(err)
+          return false
+        })
     })
     alert("Check your spotify queue")
   }
